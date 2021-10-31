92df9211b7734a9ed0210f9ffde0e0cb9115f90d
Spencer Bertsch <spencerbertsch99@gmail.com>
2021-10-30 22:03:45 -0400

    add calls to dijkstras

--- final-project/dijkstras/driver.py
+++ final-project/dijkstras/driver.py
@@ -9,7 +9,7 @@ from pathlib import Path
 import click
 import numpy as np
 
-from routines import image_reader, image_to_adjacency_list
+from routines import image_reader, image_to_adjacency_list, dijkstras, Graph
 
 random.seed(38)
 
@@ -20,17 +20,18 @@ PATH_TO_THIS_FILE: Path = Path(__file__).resolve()
 @click.option('--image_name', default=None, help='Name of the image that will be used for Dijkstras.')
 def driver(image_name: str):
 
-    print(f'Currently running Dijkstras on {image_name}')
+    print(f'Running Shortest Path analysis on: {image_name}')
     ABSPATH_TO_IMG: Path = PATH_TO_THIS_FILE.parent / 'images' / image_name
 
     # STEP 1. Read a chosen image
     image: np.ndarray = image_reader(ABSPATH_TO_IMG=ABSPATH_TO_IMG)
 
     # STEP 2: Convert the image to an adjacency list
-    adj_list: dict = image_to_adjacency_list(img=image, distance=1, use_bresenhams=False)
+    adj_list: dict = image_to_adjacency_list(img=image, distance=1, use_bresenhams=False, weight_calc='euclidean')
 
     # STEP 3: Runs dijkstras to find the shortest path
-    # TODO
+    g = Graph(adj_list=adj_list)
+    g.dijkstra(start_location=(0, 0), end_location=(6, 6))
 
     # STEP 4: Save a png of the image with the path superimposed
     # TODO - massage the path into a list of 4-length tuples (x1, y1, x2, y2)
@@ -42,6 +43,8 @@ def driver(image_name: str):
 
     # STEP 5: creates an animation of the path being generated
     # TODO
+
+    print('something')
 
 
 if __name__ == "__main__":
